58ed26d9278f0c41e89c8783690c2bd217c23f6e
mohinoddin <mohinoddin.s@robotspace.in>
2022-11-17 10:55:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Search from './Components/Search';
+import { createContext, useEffect, useState } from 'react';
+import Input from './Components/Input';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import {HiShare} from "react-icons/hi"
+export const Context = createContext();
+
+function App() {
+  const [user,setUser] = useState({});
+  const [set,setSet] = useState(new Set());
+  const [arr,setArr] = useState([]);
+  const [display,setDisplay] = useState("none");
+  useEffect(()=>{
+    
+      if(set.has(user.name)||!user.name) return;
+      set.add(user.name);
+      setArr([...arr,user]);
+  
+  },[user])
+  
+  return (
+    <>
+    <Context.Provider value={{user,setUser,arr}}>
+    <div className="App">
+      <h2>Click on Share Button</h2>
+      <button onClick={()=>setDisplay("block")} id='share'>Share &nbsp;<HiShare/></button>
+      <Routes>
+      <Route path='/' element={<Input display={display}/>}/>
+      <Route path='/add' element={<Search/>} />
+      </Routes>
+    </div>
+      </Context.Provider>
+      </>
+  );
+}
+
+export default App;
